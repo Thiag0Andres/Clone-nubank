e800cde2393a50e188fdb564345dbc0776f36416
Thiag0Andres <thiagopalacios@eng.ci.ufpb.br>
2021-03-08 17:46:46 -0300

    Adicionando o componente menu ao home

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.View`
   align-items: center;
+  padding: 40px 0 30px;
 `;
 
 export const Top = styled.View`
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,47 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
 
 // Components
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
 import Tabs from "../../components/Tabs";
 
-import { Container } from "./styles";
+// Icons
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from "./styles";
 
 const Home = () => {
   return (
     <Container>
       <Header />
-      <Text></Text>
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Trasferência de R$ 20,00 recebida de Diego Schell Fernandes
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
